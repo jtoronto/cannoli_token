d2b531fb666732dff0b3addaf0b40819adf77deb
jtoronto <joseph.toronto@yahoo.com>
2021-06-23 20:23:50 -0400

    Updates

---
+++ precache-manifest.df8c431ad7075e92bbe28722d57d95cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "404a8bc20f1a8a8f963872bf7be508d4",
+    "url": "/jtoronto/cannoli_token.git/index.html"
+  },
+  {
+    "revision": "bd8269e41271906065c3",
+    "url": "/jtoronto/cannoli_token.git/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "50713ba961793bf97675",
+    "url": "/jtoronto/cannoli_token.git/static/js/2.8880436d.chunk.js"
+  },
+  {
+    "revision": "bd8269e41271906065c3",
+    "url": "/jtoronto/cannoli_token.git/static/js/main.b51969aa.chunk.js"
+  },
+  {
+    "revision": "1454fb129d53e48d3704",
+    "url": "/jtoronto/cannoli_token.git/static/js/runtime-main.ef2d1509.js"
+  }
+]);
